afe087f9b3d66d0a59adcf8c8660780672dee809
avaglica <alexis.vaglica95@gmail.com>
2026-08-07 17:01:40 -0300

    Primer Test para Gameplay

---
+++ MemoTest/MemoTestTests/MemotestGameplayTests.swift
@@ -0,0 +1,38 @@
+//
+//  MemoTestTests.swift
+//  MemoTestTests
+//
+//  Created by AVaglica on 07/08/2026.
+//
+
+import Testing
+@testable import MemoTest
+
+struct GameplayCoreTests {
+
+    @Test
+    func example() {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
+
+struct GameplayCardGeneratorTests {
+    
+    @Test
+    func test_hasExactPairs() {
+        //When
+        let generator = GameplayCardGenerator()
+        let deck = generator.generateDeck()
+        
+        //Given
+        var contentCounts: [String: Int] = [:]
+        
+        for card in deck {
+            contentCounts[card.content, default: 0] += 1
+        }
+        
+        //Then
+        #expect(contentCounts.count == 8)
+    }
+}
